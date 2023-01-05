67813338ed86d8e84c38bbd8d94fd79f5618a509
Xinyuan Lin <xinyual3@uci.edu>
2023-01-04 18:23:17 -0800

    Update qe.h

--- src/include/qe.h
+++ src/include/qe.h
@@ -91,6 +91,7 @@ namespace PeterDB {
             for (Attribute &attribute : attributes) {
                 attribute.name = tableName + "." + attribute.name;
             }
+            return 0;
         };
 
         ~TableScan() override {
